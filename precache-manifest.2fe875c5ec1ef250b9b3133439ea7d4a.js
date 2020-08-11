24da31dd70a8a4d454bd8a08fd1749540fa19e37
matheusnovais <matheusnovaisz@gmail.com>
2020-08-11 19:29:17 -0300

    Updates

---
+++ precache-manifest.2fe875c5ec1ef250b9b3133439ea7d4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86896263282cdf8b6efbda54d16c62f0",
+    "url": "/superficie/index.html"
+  },
+  {
+    "revision": "be373303e48aadbb986c",
+    "url": "/superficie/static/css/main.c05e67f0.chunk.css"
+  },
+  {
+    "revision": "f798280db68a3efc8857",
+    "url": "/superficie/static/js/2.e9c1cead.chunk.js"
+  },
+  {
+    "revision": "4651a60dd24a3d76949974fa9e435183",
+    "url": "/superficie/static/js/2.e9c1cead.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be373303e48aadbb986c",
+    "url": "/superficie/static/js/main.3593f770.chunk.js"
+  },
+  {
+    "revision": "740b61721cce16a13598",
+    "url": "/superficie/static/js/runtime-main.7824b015.js"
+  },
+  {
+    "revision": "98ab8955c9fbc5dd0afe7f3b924f60c8",
+    "url": "/superficie/static/media/hand.98ab8955.jpg"
+  }
+]);
